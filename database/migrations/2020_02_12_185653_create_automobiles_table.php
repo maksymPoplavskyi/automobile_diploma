760b0a1e0606b197980795040c3b7e364cf51d9e
root <root@php.php>
2020-02-12 18:58:45 +0000

    create migrations

---
+++ database/migrations/2020_02_12_185653_create_automobiles_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAutomobilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('automobiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedSmallInteger('model_id');
+            $table->unsignedSmallInteger('year');
+            $table->unsignedMediumInteger('mileage');
+            $table->unsignedBigInteger('location_id');
+            $table->unsignedTinyInteger('damage_id');
+            $table->unsignedInteger('current_rate');
+            $table->unsignedTinyInteger('transmission_id');
+            $table->unsignedBigInteger('additional_id');
+            $table->uuid('VIN');
+            $table->timestamps();
+            $table->integer('status');
+
+            $table->foreign('model_id')
+                ->references('id')
+                ->on('models')
+                ->onDelete('cascade');
+
+            $table->foreign('location_id')
+                ->references('id')
+                ->on('locations')
+                ->onDelete('cascade');
+
+            $table->foreign('damage_id')
+                ->references('id')
+                ->on('damages')
+                ->onDelete('cascade');
+
+            $table->foreign('transmission_id')
+                ->references('id')
+                ->on('transmissions')
+                ->onDelete('cascade');
+
+            $table->foreign('additional_id')
+                ->references('id')
+                ->on('additionals')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('automobiles');
+    }
+}
